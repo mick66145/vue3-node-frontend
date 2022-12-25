aa9f8ed23ee13c8b2122e514fdacc67dd3dbf280
ming <ming@acube.com.tw>
2022-12-25 10:26:37 +0800

    feat(useCRUD):
    - api error show notify
    - 新增readListFetch

--- src/use/useCRUD.js
+++ src/use/useCRUD.js
@@ -19,19 +19,26 @@ export default function useCRUD ({
   deleteSuccess = '刪除成功',
   deleteFail = '刪除失敗',
 
+  readListFetch,
+  readListSuccess = '讀取列表成功',
+  readListFail = '讀取列表失敗',
+
 }) {
-  const { notify } = useNotify()
+  const { notify, notifyAPIError } = useNotify()
 
   const reqCreate = useAsyncState(createFetch, {}, { immediate: false })
   const reqRead = useAsyncState(readFetch, {}, { immediate: false })
   const reqUpdate = useAsyncState(updateFetch, {}, { immediate: false })
   const reqDelete = useAsyncState(deleteFetch, {}, { immediate: false })
+  const reqReadList = useAsyncState(readListFetch, {}, { immediate: false })
 
   const form = ref()
 
   const callCreateFetch = async (payload) => {
     const res = await reqCreate.execute(0, payload)
     if (reqCreate.error.value) {
+      const message = reqCreate.error.value.response.data.message
+      notifyAPIError({ message })
       return [null, reqCreate.error.value]
     } else {
       notify({ message: createSuccess, type: 'positive' })
@@ -43,6 +50,8 @@ export default function useCRUD ({
     console.log('🚀 ~ callReadFetch ~ payload', id, payload)
     const res = await reqRead.execute(0, id, payload)
     if (reqRead.error.value) {
+      const message = reqRead.error.value.response.data.message
+      notifyAPIError({ message })
       return [null, reqRead.error.value]
     } else {
       return [res, null]
@@ -53,6 +62,8 @@ export default function useCRUD ({
     const res = await reqUpdate.execute(0, id, payload)
     console.log('🚀 ~ callUpdateFetch ~ res', res)
     if (reqUpdate.error.value) {
+      const message = reqUpdate.error.value.response.data.message
+      notifyAPIError({ message })
       return [null, reqUpdate.error.value]
     } else {
       notify({ message: updateSuccess, type: 'positive' })
@@ -64,10 +75,24 @@ export default function useCRUD ({
     const res = await reqDelete.execute(0, id)
     console.log('🚀 ~ callDeleteFetch ~ res', res)
     if (reqDelete.error.value) {
+      const message = reqDelete.error.value.response.data.message
+      notifyAPIError({ message })
       return [null, reqDelete.error.value]
     } else {
       notify({ message: deleteSuccess, type: 'positive' })
       return [res || true, null]
+    }
+  }
+
+  const callReadListFetch = async (payload = null) => {
+    console.log('🚀 ~ callReadListFetch ~ payload', payload)
+    const res = await reqReadList.execute(0, payload)
+    if (reqReadList.error.value) {
+      const message = reqReadList.error.value.response.data.message
+      notifyAPIError({ message })
+      return [null, reqReadList.error.value]
+    } else {
+      return [res, null]
     }
   }
 
@@ -77,5 +102,6 @@ export default function useCRUD ({
     callReadFetch,
     callUpdateFetch,
     callDeleteFetch,
+    callReadListFetch,
   }
 }
