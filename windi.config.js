7341cd19d1c1c6406df52de5539d1cfe4cace713
ming <ming@acube.com.tw>
2023-01-09 11:11:40 +0800

    feat: screen設定

--- windi.config.js
+++ windi.config.js
@@ -25,11 +25,11 @@ export default defineConfig({
       },
     },
     screens: {
-      xs: '0px',
-      sm: '600px',
-      md: '1024px',
-      lg: '1440px',
-      xl: '1920px',
+      xs: '576px',
+      sm: '768px',
+      md: '992px',
+      lg: '1200px',
+      xl: '1440px',
     },
   },
   plugins: [
