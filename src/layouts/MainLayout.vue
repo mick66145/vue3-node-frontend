8b58f9f22b574b68dd3b66b3d928c32720d7f3ff
ming <mick66145@gmail.com>
2023-03-07 23:45:30 +0800

    feat: layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <app-header />
+    <q-page-container class="bg-grey-1">
+      <router-view />
+    </q-page-container>
+    <app-footer />
+  </q-layout>
+</template>
+
+<script>
+import { AppHeader, AppFooter } from './components'
+import { defineComponent } from 'vue-demi'
+
+export default defineComponent({
+  name: 'MainLayout',
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+  setup () {
+
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+/* Layout */
+import MainLayout from '@/layouts/MainLayout.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: '/front',
+      component: MainLayout,
       children: [
         {
-          path: 'front',
+          path: '',
           component: () => import('@/views/front/Front.vue'),
           name: 'Front',
           meta: { title: 'front' },
