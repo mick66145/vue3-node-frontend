f9671d322cedf476658044d45f55bfc8895d8526
ming <mick66145@gmail.com>
2023-03-07 22:08:14 +0800

    feat: 同步use

--- src/use/useCRUD.js
+++ src/use/useCRUD.js
@@ -1,6 +1,7 @@
 import { useAsyncState } from '@vueuse/core'
-import { ref } from 'vue-demi'
+import { ref, computed } from 'vue-demi'
 import useNotify from './useNotify'
+import { useApp } from '@/stores/app'
 
 export default function useCRUD ({
   createFetch,
@@ -25,7 +26,7 @@ export default function useCRUD ({
 
 }) {
   const { notify, notifyAPIError } = useNotify()
-
+  const storeApp = useApp()
   const reqCreate = useAsyncState(createFetch, {}, { immediate: false })
   const reqRead = useAsyncState(readFetch, {}, { immediate: false })
   const reqUpdate = useAsyncState(updateFetch, {}, { immediate: false })
@@ -35,51 +36,63 @@ export default function useCRUD ({
   const form = ref()
 
   const callCreateFetch = async (id = null, payload) => {
+    storeApp.isLoading = true
     const res = await reqCreate.execute(0, id, payload)
     if (reqCreate.error.value) {
-      const message = reqCreate.error.value.response.data.message
+      storeApp.isLoading = false
+      const message = reqCreate.error.value.response.data.message || reqCreate.error.value
       notifyAPIError({ message })
       return [null, reqCreate.error.value]
     } else {
       notify({ message: createSuccess, type: 'positive' })
+      storeApp.isLoading = false
       return [res, null]
     }
   }
 
   const callReadFetch = async (id, payload = null) => {
+    storeApp.isLoading = true
     console.log('🚀 ~ callReadFetch ~ payload', id, payload)
     const res = await reqRead.execute(0, id, payload)
     if (reqRead.error.value) {
-      const message = reqRead.error.value.response.data.message
+      storeApp.isLoading = false
+      const message = reqRead.error.value.response.data.message || reqRead.error.value
       notifyAPIError({ message })
       return [null, reqRead.error.value]
     } else {
+      storeApp.isLoading = false
       return [res, null]
     }
   }
 
   const callUpdateFetch = async (id, payload = null) => {
+    storeApp.isLoading = true
     const res = await reqUpdate.execute(0, id, payload)
     console.log('🚀 ~ callUpdateFetch ~ res', res)
     if (reqUpdate.error.value) {
-      const message = reqUpdate.error.value.response.data.message
+      storeApp.isLoading = false
+      const message = reqUpdate.error.value.response.data.message || reqUpdate.error.value
       notifyAPIError({ message })
       return [null, reqUpdate.error.value]
     } else {
       notify({ message: updateSuccess, type: 'positive' })
+      storeApp.isLoading = false
       return [res || true, null]
     }
   }
 
   const callDeleteFetch = async (id) => {
+    storeApp.isLoading = true
     const res = await reqDelete.execute(0, id)
     console.log('🚀 ~ callDeleteFetch ~ res', res)
     if (reqDelete.error.value) {
-      const message = reqDelete.error.value.response.data.message
+      storeApp.isLoading = false
+      const message = reqDelete.error.value.response.data.message || reqDelete.error.value
       notifyAPIError({ message })
       return [null, reqDelete.error.value]
     } else {
       notify({ message: deleteSuccess, type: 'positive' })
+      storeApp.isLoading = false
       return [res || true, null]
     }
   }
@@ -88,7 +101,7 @@ export default function useCRUD ({
     console.log('🚀 ~ callReadListFetch ~ payload', payload)
     const res = await reqReadList.execute(0, id, payload)
     if (reqReadList.error.value) {
-      const message = reqReadList.error.value.response.data.message
+      const message = reqReadList.error.value.response.data.message || reqReadList.error.value
       notifyAPIError({ message })
       return [null, reqReadList.error.value]
     } else {
@@ -96,8 +109,11 @@ export default function useCRUD ({
     }
   }
 
+  const isLoading = computed(() => reqCreate.isLoading.value || reqRead.isLoading.value || reqUpdate.isLoading.value || reqDelete.isLoading.value || reqReadList.isLoading.value)
+
   return {
     form,
+    isLoading,
     callCreateFetch,
     callReadFetch,
     callUpdateFetch,
