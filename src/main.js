0ebb51e435d99a6551306aef99e7a96c99ea858a
ming <mick66145@gmail.com>
2023-03-07 22:07:44 +0800

    feat: plugins-quasar

--- src/main.js
+++ src/main.js
@@ -1,32 +1,29 @@
 import { createApp } from 'vue'
-import { Quasar } from 'quasar'
 import { createPinia } from 'pinia'
 
-// quasar
-import 'quasar/src/css/index.sass'
-import '@quasar/extras/material-icons/material-icons.css'
+import quasar from '@/plugins/quasar'
 
 // windi
 import 'virtual:windi.css'
 
 import App from './App.vue'
 import router from './router'
-import { useDayjs } from '@/plugins/dayjs'
 import GlobalComponents from './components'
 
 import 'virtual:svg-icons-register'
 import 'quill/dist/quill.snow.css'
 import '@/styles/index.scss'
 
+// plugins
+import { useDayjs } from '@/plugins/dayjs'
 import i18n from '@/plugins/i18n'
 import { useVuelidate } from '@/plugins/vuelidate'
 
 const app = createApp(App)
-app.use(Quasar)
+app.use(quasar)
 app.use(createPinia())
 app.use(router)
 app.use(GlobalComponents)
-
 app.use(useDayjs)
 app.use(i18n)
 app.use(useVuelidate)
