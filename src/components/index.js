1ffedf9ee63d43d9ca3f49746779fe4754765c73
ming <mick66145@gmail.com>
2023-03-07 21:55:03 +0800

    feat: calendar

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,6 @@ import InputSelect from './form/InputSelect.vue'
 import InputMultipleSelect from './form/InputMultipleSelect.vue'
 import InputPassword from './form/InputPassword.vue'
 import InputDate from './form/InputDate.vue'
-import InputDialog from './form/InputDialog.vue'
 import InputImageUpload from './form/InputImageUpload.vue'
 import InputImage from './form/InputImage.vue'
 import InputOptionGroup from './form/InputOptionGroup.vue'
@@ -13,8 +12,17 @@ import InputToggle from './form/InputToggle.vue'
 import InputSearch from './form/InputSearch.vue'
 import InputTextarea from './form/InputTextarea.vue'
 import InputCheckbox from './form/InputCheckbox.vue'
+import InputTime from './form/InputTime.vue'
+import InputNumber from './form/InputNumber.vue'
+import InputMonth from './form/InputMonth/InputMonth.vue'
+import InputEmail from './form/InputEmail.vue'
 import BaseButton from './button/BaseButton.vue'
+import BaseImage from './image/BaseImage.vue'
+import BaseDialog from './dialog/BaseDialog.vue'
+import ImagePreviewDialog from './dialog/ImagePreviewDialog.vue'
+import MessageDialog from './dialog/MessageDialog.vue'
 import Pagination from './Pagination.vue'
+import Calendar from './Calendar.vue'
 export default {
   install (app) {
     app.component('SvgIcon', SvgIcon)
@@ -23,7 +31,6 @@ export default {
     app.component('InputMultipleSelect', InputMultipleSelect)
     app.component('InputPassword', InputPassword)
     app.component('InputDate', InputDate)
-    app.component('InputDialog', InputDialog)
     app.component('InputImageUpload', InputImageUpload)
     app.component('InputImage', InputImage)
     app.component('InputOptionGroup', InputOptionGroup)
@@ -32,7 +39,16 @@ export default {
     app.component('InputSearch', InputSearch)
     app.component('InputTextarea', InputTextarea)
     app.component('InputCheckbox', InputCheckbox)
+    app.component('InputTime', InputTime)
+    app.component('InputNumber', InputNumber)
+    app.component('InputMonth', InputMonth)
+    app.component('InputEmail', InputEmail)
     app.component('BaseButton', BaseButton)
+    app.component('BaseImage', BaseImage)
+    app.component('BaseDialog', BaseDialog)
+    app.component('ImagePreviewDialog', ImagePreviewDialog)
+    app.component('MessageDialog', MessageDialog)
     app.component('Pagination', Pagination)
+    app.component('Calendar', Calendar)
   },
 }
