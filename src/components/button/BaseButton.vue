c80cd2635e735d5456a236a6aa81032414bed25a
ming <mick66145@gmail.com>
2023-05-04 17:24:07 +0800

    feat(BaseButton): isLoading、size

--- src/components/button/BaseButton.vue
+++ src/components/button/BaseButton.vue
@@ -6,13 +6,20 @@
     :color="color"
     :label="label"
     :icon="icon"
+    :loading="isLoading"
+    :size="size"
   >
     <slot />
+    <template #loading>
+      <q-spinner-hourglass size="0.75em" />
+    </template>
   </q-btn>
 </template>
 
 <script>
-import { defineComponent } from 'vue-demi'
+import { defineComponent, computed } from 'vue-demi'
+import { useApp } from '@/stores/app'
+
 export default defineComponent({
   props: {
     label: { type: String, default: '' },
@@ -20,9 +27,18 @@ export default defineComponent({
     icon: { type: String },
     outline: { type: Boolean, default: false },
     rounded: { type: Boolean, default: false },
+    size: { type: String, default: 'md' },
   },
   setup () {
+    // data
+    const storeApp = useApp()
+
+    const isLoading = computed(() => {
+      return storeApp.isLoading
+    })
+
     return {
+      isLoading,
     }
   },
 })
