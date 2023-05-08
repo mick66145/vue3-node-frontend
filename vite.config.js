2ba39a6e38430a91f7c5468bb78839b0c58754db
ming <mick66145@gmail.com>
2023-05-08 13:19:56 +0800

    chore: pwa

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable node/no-path-concat */
 import { fileURLToPath, URL } from 'url'
-
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -8,24 +7,9 @@ import WindiCSS from 'vite-plugin-windicss'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
 import stylelint from 'vite-plugin-stylelint'
-
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
 import mkcert from 'vite-plugin-mkcert'
-const fs = require('fs')
-
-const https = () => {
-  const useHttps = (process.env.npm_lifecycle_event || '').includes(':https')
-  const keyPem = `${__dirname}/localhost-key.pem`
-  const certPem = `${__dirname}/localhost.pem`
-  if (!useHttps) return {}
-  if (!fs.existsSync(keyPem) || !fs.existsSync(certPem)) return {}
-  return {
-    https: {
-      key: fs.readFileSync(keyPem),
-      cert: fs.readFileSync(certPem),
-    },
-  }
-}
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 const root = process.cwd()
@@ -54,11 +38,44 @@ export default defineConfig(({ command, mode }) => {
         compositionOnly: false,
         include: path.resolve(__dirname, 'src/locales/**'),
       }),
+      VitePWA({
+        includeAssets: ['favicon.svg'],
+        manifest: false,
+        registerType: 'autoUpdate',
+        workbox: {
+          runtimeCaching: [
+            {
+              urlPattern: /someInterface/i, // 接口存儲此處填寫你想存儲的接口正則匹配
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'interface-cache',
+              },
+            },
+            {
+              urlPattern: /(.*?)\.(js|css|ts)/, // js /css /ts靜態資源保存
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'js-css-cache',
+              },
+            },
+            {
+              urlPattern: /(.*?)\.(png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/, // 圖片存檔
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'image-cache',
+              },
+            },
+          ],
+        },
+      }),
     ],
     server: {
       https: false,
       cors: true,
       port: 3000,
+      hmr: {
+        port: 3000,
+      },
     },
     resolve: {
       alias: {
