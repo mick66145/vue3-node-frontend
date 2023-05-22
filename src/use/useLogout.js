28409fc1ee17bad35b209608f1a9c73470523787
ming <mick66145@gmail.com>
2023-05-22 14:32:36 +0800

    feat: useLogout

---
+++ src/use/useLogout.js
@@ -0,0 +1,18 @@
+import { useUser } from '@/stores/user'
+import { useRouter } from 'vue-router'
+import useNotify from './useNotify'
+export default function useLogout () {
+  const router = useRouter()
+  const { notify } = useNotify()
+  const storeUser = useUser()
+
+  const resetStore = async () => {
+    await storeUser.logout()
+    notify({ message: '登出成功', type: 'positive' })
+    router.push('/')
+  }
+
+  return {
+    resetStore,
+  }
+}
