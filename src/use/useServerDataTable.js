56412da73dbfb101117fc2ff49f7f05345089612
ming <mick66145@gmail.com>
2023-05-24 17:37:10 +0800

    feat(useServerDataTable): 同步

--- src/use/useServerDataTable.js
+++ src/use/useServerDataTable.js
@@ -1,31 +1,24 @@
-import { reactive, ref } from 'vue-demi'
+import { reactive, ref, onMounted } from 'vue-demi'
 import useSessionStorage from './useSessionStorage'
+import mapKeys from 'lodash-es/mapKeys'
 
 export default function useServerDataTable ({
   searchParames = {},
+  unSessionStorageParames = [], // [{field:string}]
   sessionStorageKey = 'clientFrontendServerDataTable',
   callback = () => {},
 }) {
+  // use
   const { setSessionStorage, getSessionStorage } = useSessionStorage()
+
+  // data
   let sessionStorage = getSessionStorage(sessionStorageKey)
-  if (!sessionStorage) {
-    const sessionStorageObj = {
-      search: {
-        page: 1,
-        page_size: 10,
-      },
-    }
-    setSessionStorage(sessionStorageKey, sessionStorageObj)
-    sessionStorage = getSessionStorage(sessionStorageKey)
-  }
-  for (const [key, value] of Object.entries(searchParames)) {
-    !sessionStorage.search[key] && (sessionStorage.search[key] = value)
-    key === 'page_size' && (sessionStorage.search[key] = value)
-  }
-  setSessionStorage(sessionStorageKey, sessionStorage)
-  const search = reactive(sessionStorage.search)
+  const search = reactive({})
   const data = ref([])
   const total = ref(0)
+  const unSessionStorageParamesField = unSessionStorageParames.map((item) => item.field)
+
+  // methods
 
   const onChangePage = (page) => {
     search.page = page
@@ -34,7 +27,13 @@ export default function useServerDataTable ({
       callback()
     }
   }
-
+  const onChangePageSize = (pageSize) => {
+    search.page_size = pageSize
+    setSessionStorage(sessionStorageKey, { search })
+    if (callback && typeof (callback) === 'function') {
+      callback()
+    }
+  }
   const onChangeFilter = () => {
     search.page = 1
     setSessionStorage(sessionStorageKey, { search })
@@ -43,19 +42,56 @@ export default function useServerDataTable ({
     }
   }
 
-  const onReset = () => {
+  const onReset = async () => {
+    for (const [key, value] of Object.entries(searchParames)) {
+      search[key] = value
+    }
     search.page = 1
+    search.page_size = 10
     setSessionStorage(sessionStorageKey, { search })
     if (callback && typeof (callback) === 'function') {
-      callback()
+      await callback()
     }
   }
+
+  // mounted
+  onMounted(async () => {
+    if (!sessionStorage) {
+      const sessionStorageObj = {
+        search: {
+          page: 1,
+          page_size: 10,
+        },
+      }
+      setSessionStorage(sessionStorageKey, sessionStorageObj)
+      sessionStorage = getSessionStorage(sessionStorageKey)
+    }
+
+    mapKeys(sessionStorage.search, (_, key) => {
+      search[key] = sessionStorage.search[key]
+    })
+
+    for (const [key, value] of Object.entries(searchParames)) {
+      (!sessionStorage.search[key] && !unSessionStorageParamesField.includes(key)) && (search[key] = value)
+    }
+
+    setSessionStorage(sessionStorageKey, { search })
+
+    for (const [key, value] of Object.entries(searchParames)) {
+      (unSessionStorageParamesField.includes(key)) && (search[key] = value)
+    }
+
+    if (callback && typeof (callback) === 'function') {
+      await callback()
+    }
+  })
 
   return {
     search,
     data,
     total,
     onChangePage,
+    onChangePageSize,
     onChangeFilter,
     onReset,
   }
