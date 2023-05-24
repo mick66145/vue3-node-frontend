6cc6a2e3c77319fb886e79332bbf4950c85e1dfb
ming <mick66145@gmail.com>
2023-05-24 09:21:49 +0800

    feat(request): defaultRequestInterceptors

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,38 @@
+/* eslint-disable no-unused-expressions */
 import Configuration from '@/configuration'
 import axios from 'axios'
+import qs from 'qs'
+import { useUser } from '@/stores/user'
+import { getToken } from '@/utils/auth'
+
+const defaultRequestInterceptors = (config) => {
+  const storeUser = useUser()
+  if (config.method === 'post' && (config.headers)['Content-Type'] === 'application/x-www-form-urlencoded') {
+    config.data = qs.stringify(config.data)
+  }
+  if (config.method === 'get' && config.params) {
+    let url = config.url
+    url += '?'
+    const keys = Object.keys(config.params)
+    for (const key of keys) {
+      if (config.params[key] !== undefined && config.params[key] !== null) {
+        url += `${key}=${encodeURIComponent(config.params[key])}&`
+      }
+    }
+    url = url.substring(0, url.length - 1)
+    config.params = {}
+    config.url = url
+  }
+  if (storeUser.token) {
+    config.headers['X-Token'] = getToken()
+    config.headers.Authorization = `Bearer ${getToken()}`
+  }
+  return config
+};
+(error) => {
+  console.log(error)
+  Promise.reject(error)
+}
 
 export const handleError = (error) => {
   const { response } = error
@@ -10,10 +43,22 @@ export const handleError = (error) => {
   // can not receive response
   // 請求過時或者是斷網
   if (!window.navigator.onLine) {
+    // show notify
   } else {
     // maybe Program have some problem
     return Promise.reject(error)
   }
+}
+
+export const handleAuthError = async (error) => {
+  const { status } = error.response
+  if (status !== 401) return Promise.reject(error)
+  // do logout
+  return Promise.reject(error)
+}
+
+export const handleResponse = async (response) => {
+  return Promise.resolve(response)
 }
 
 // create an axios instance
@@ -24,19 +69,16 @@ const service = axios.create({
 })
 
 // request interceptor
-service.interceptors.request.use(
-  config => {
-    return config
-  },
-  error => {
-    // do something with request error
-    return Promise.reject(error)
-  },
+service.interceptors.request.use(defaultRequestInterceptors)
+
+service.interceptors.response.use(
+  handleResponse,
+  handleError,
 )
 
 service.interceptors.response.use(
-  response => response,
-  handleError,
+  handleResponse,
+  handleAuthError,
 )
 
 export default service
