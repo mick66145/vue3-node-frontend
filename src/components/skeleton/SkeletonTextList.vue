eba453c2ca72a37289099d46e65bf540a6de65c8
ming <mick66145@gmail.com>
2023-05-30 13:45:54 +0800

    refactor(SkeletonTextList): use skeleton-text-item

--- src/components/skeleton/SkeletonTextList.vue
+++ src/components/skeleton/SkeletonTextList.vue
@@ -1,38 +1,7 @@
 
 <template>
   <template v-for="n in 5" :key="n">
-    <q-item v-if="n%2===0" class="q-pa-md">
-      <q-item-section>
-        <q-item-label>
-          <q-skeleton type="text" />
-        </q-item-label>
-        <q-item-label>
-          <q-skeleton type="text" width="80%" />
-        </q-item-label>
-        <q-item-label>
-          <q-skeleton type="text" width="65%" />
-        </q-item-label>
-        <q-item-label>
-          <q-skeleton type="text" width="40%" />
-        </q-item-label>
-      </q-item-section>
-    </q-item>
-    <q-item v-if="n%2===1" class="q-pa-md">
-      <q-item-section>
-        <q-item-label>
-          <q-skeleton type="text" />
-        </q-item-label>
-        <q-item-label>
-          <q-skeleton type="text" width="30%" />
-        </q-item-label>
-        <q-item-label>
-          <q-skeleton type="text" width="70%" />
-        </q-item-label>
-        <q-item-label>
-          <q-skeleton type="text" width="10%" />
-        </q-item-label>
-      </q-item-section>
-    </q-item>
+    <skeleton-text-item class="q-pa-md" />
   </template>
 </template>
 <script>
