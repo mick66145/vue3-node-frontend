2d7834f309c03853e746d356d542bda1f9ded509
ming <mick66145@gmail.com>
2023-06-13 11:55:24 +0800

    feat(request): 設定withCredentials=true

--- src/utils/request.js
+++ src/utils/request.js
@@ -64,7 +64,7 @@ export const handleResponse = async (response) => {
 // create an axios instance
 const service = axios.create({
   baseURL: `${Configuration('backendHost')}`, // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
+  withCredentials: true, // send cookies when cross-domain requests
   timeout: 600000, // request timeout
 })
 
