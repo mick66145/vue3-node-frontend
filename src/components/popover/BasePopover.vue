f1117e9cb7716bae48a114d2ada90f845ceb9390
ming <mick66145@gmail.com>
2023-06-16 09:51:40 +0800

    feat(BasePopover): props新增trigger

--- src/components/popover/BasePopover.vue
+++ src/components/popover/BasePopover.vue
@@ -18,11 +18,12 @@
 import { defineComponent, computed, toRefs } from 'vue-demi'
 export default defineComponent({
   props: {
-    anchor: { type: String, default: 'top middle' },
-    self: { type: String, default: 'bottom middle' },
+    anchor: { type: String, default: 'bottom middle' },
+    self: { type: String, default: 'top middle' },
     transitionShow: { type: String, default: 'scale' },
     transitionHide: { type: String, default: 'scale' },
     offset: { type: Array },
+    trigger: { type: String, default: 'hover' },
     color: { type: String, default: 'white' },
     textColor: { type: String, default: 'black' },
   },
