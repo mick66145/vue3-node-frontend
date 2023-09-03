d601516a37cd49effc80b04316784d5b08a2e807
ming <mick66145@gmail.com>
2023-09-03 09:07:07 +0800

    refactor: api

--- src/api/restful/modules/basic-base/index.js
+++ src/api/restful/modules/basic-base/index.js
@@ -1,7 +1,13 @@
+import city from './modules/city'
+import area from './modules/area'
+import auth from './modules/auth'
 import file from './modules/file'
 
 export const exportBaseApiModules = () => {
   const modules = {}
+  modules.CityResource = city
+  modules.AreaResource = area
+  modules.AuthResource = auth
   modules.FileResource = file
   return modules
 }
