8e903af63a763605e744006c64ed3fe9f3c92517
ming <mick66145@gmail.com>
2023-09-25 16:26:17 +0800

    refactor(swipeable-carousel): options

--- src/components/carousel/SwipeableCarousel.vue
+++ src/components/carousel/SwipeableCarousel.vue
@@ -2,17 +2,10 @@
   <base-carousel
     :transition-prev="transitionPrev"
     :transition-next="transitionNext"
+    :options="options"
     swipeable
   >
     <slot name="default" />
-    <template v-if="options.length>0">
-      <q-carousel-slide
-        v-for="(carouselItem,index) in options"
-        :key="index"
-        :name="index"
-        :img-src="carouselItem.img_src"
-      />
-    </template>
   </base-carousel>
 </template>
 
@@ -23,7 +16,7 @@ export default defineComponent({
   props: {
     transitionPrev: { type: String, default: 'slide-right' },
     transitionNext: { type: String, default: 'slide-left' },
-    options: { type: Array, default () { /* { img_src:"" } */ return [] } },
+    options: { type: Array, default () { return [] } },
   },
   setup (props, { emit }) {
     return {
