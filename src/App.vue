649a8f16d0f92921ca26e9f5c2c147a1fbeb2d01
ming <mick66145@gmail.com>
2023-10-10 08:06:51 +0800

    feat(App): 新增置底浮水印

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,39 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue-demi'
+import { defineComponent, onMounted, onBeforeUnmount } from 'vue-demi'
 import { RouterView } from 'vue-router'
 import useMetaInfo from '@/hooks/useMetaInfo'
+import useWatermark from '@/hooks/useWatermark'
+import useScreen from '@/hooks/useScreen'
 
 export default defineComponent({
   setup () {
-    useMetaInfo()
+    // mounted
+    onMounted(() => {
+      useMetaInfo()
+      show()
+    })
+
+    onBeforeUnmount(() => { hide() })
+
+    // use
+    const { screenWidth } = useScreen({})
+    const { show, hide } = useWatermark({
+      text: '',
+      fontColor: 'red',
+      rotate: 0,
+      translateX: -35,
+      translateY: 20,
+      width: 150,
+      height: 50,
+      fontSize: 50,
+      offsetBottom: '0',
+      offsetLeft: `${screenWidth.value < 1440 ? (30 + screenWidth.value / 100) : 50}%`,
+      watermarkWidth: '150px',
+      watermarkHeight: '50px',
+    })
+
     return {
       RouterView,
     }
